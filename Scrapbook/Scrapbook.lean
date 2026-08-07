40e4a2c0a47bc14a603df20b95f1acbd6329c9c5
Frank Jung <frankhjung@linux.com>
2026-08-07 14:48:26 +1000

    feat: add Structure module

--- Scrapbook/Scrapbook.lean
+++ Scrapbook/Scrapbook.lean
@@ -10,16 +10,19 @@ namespace Scrapbook
 /-!
 # Scrapbook
 
-A work scrapbook while learning [Mathematics in Lean](https://leanprover-community.github.io/mathematics_in_lean)
+A work scrapbook while learning [Mathematics in
+Lean](https://leanprover-community.github.io/mathematics_in_lean)
 
 -/
 
 /-! Function $f (x) = 3x$ -/
 def f (x : ℕ) := 3 * x
 
--- #eval f 1 = 3
+/-! Evaluate function $f$ at $x = 1$. -/
+#eval f 1 = 3
+-- true
 
-/-! Equivalent to f above. -/
+/-! Equivalent to $f$ above. -/
 #check fun (x : ℕ) => 3 * x
 -- fun x => 3 * x : ℕ → ℕ
 
@@ -29,20 +32,20 @@ def f (x : ℕ) := 3 * x
 #eval (λ x : ℕ => 3 * x) 12
 -- 36
 
-
-/-!
-  Simple test of function $f (1) = 3$
--/
+/-! Simple test of function $f (1) = 3$ -/
 theorem f1_eq_3 : f 1 = 3 := rfl
 
--- #check f1_eq_3
+#check f1_eq_3
+-- f1_eq_3 : f 1 = 3
 
 /-!
-  **Examples:** An *example* is an anonymous definition that is elaborated and then discarded.
+## Example
+
+An *example* is an anonymous definition that is elaborated and then discarded.
 -/
 
 /-!
-  Even numbers: $m × even$ is still even.
+Even numbers: $m × even$ is still even.
 -/
 example : ∀ m n : ℕ, Even n → Even (m * n) :=
   fun m n ⟨k, (hk : n = k + k)⟩ ↦
@@ -50,17 +53,19 @@ example : ∀ m n : ℕ, Even n → Even (m * n) :=
   show ∃ l, m * n = l + l from ⟨_, hmn⟩
 
 /-!
-  Same proof compressed to one line.
-  Here, `mul_add` is a standard theorem for distributing multiplication over
-  addition. It states that for elements `a`, `b`, and `c`,
-  `\(a × (b + c) = (a × b) + (a × c)`.
-  It acts as a shortcut for expanding brackets.
+
+Same proof compressed to one line.
+
+Here, `mul_add` is a standard theorem for distributing multiplication over
+addition. It states that for elements `a`, `b`, and `c`, `\(a × (b + c) = (a ×
+b) + (a × c)`. It acts as a shortcut for expanding brackets.
+
 -/
 example : ∀ m n : ℕ, Even n → Even (m * n) :=
   fun m n ⟨k, hk⟩ ↦ ⟨m * k, by rw [hk, mul_add]⟩
 
 /-!
-  We can also use tactics to prove this.
+We can also use tactics to prove this.
 -/
 example : ∀ m n : ℕ, Even n → Even (m * n) := by
   -- Say `m` and `n` are natural numbers,
@@ -77,27 +82,34 @@ example : ∀ m n : ℕ, Even n → Even (m * n) := by
   ring
 
 /-!
-  Same proof compressed to one line using `ring` tactic.
-  Which is just the above one line proof compressed with  semicolons.
+Same proof compressed to one line using `ring` tactic. Which is just the above
+one line proof compressed with  semicolons.
 
-  `rintro` is a shorthand for the tactics `intros` and `rcases`.
-  Where:
-  - `intros` repeatedly applies `intro` to introduce hypotheses
-  - `rcases` performs `cases` recursively
-  - `cases` splits goal into each case
+`rintro` is a shorthand for the tactics `intros` and `rcases`.
 
-  Note: semicolons can be used to separate tactics in a proof
+Where:
+- `intros` repeatedly applies `intro` to introduce hypotheses
+- `rcases` performs `cases` recursively
+- `cases` splits goal into each case
+
+**Notes**
+
+- Semicolons can be used to separate tactics in a proof
+
 -/
 example : ∀ m n : ℕ, Even n → Even (m * n) := by
   rintro m n ⟨k, hk⟩; use m * k; rw [hk]; ring
 
 /-!
-  Lean has a simplifier tactic that can prove this automatically:
-  Where:
-  - `simp` tactic uses lemmas and hypotheses to simplify the main
-     goal target or non-dependent hypotheses
-  - `parity_simps` (Mathlib) is a specialized collection of
-     mathematical theorems used with the `simp` tactic
+
+Lean has a simplifier tactic that can prove this automatically:
+
+Where:
+- `simp` tactic uses lemmas and hypotheses to simplify the main
+   goal target or non-dependent hypotheses
+- `parity_simps` (Mathlib) is a specialized collection of
+   mathematical theorems used with the `simp` tactic
+
 -/
 example : ∀ m n : ℕ, Even n → Even (m * n) := by
   intros; simp [*, parity_simps]
@@ -116,15 +128,19 @@ example (a b c : ℝ) : a * (b * c) = b * (c * a) := by
   rw [mul_comm]
   rw [mul_assoc]
 
-/- Ring example -/
+/-! Ring example -/
 example (R : Type*) [CommRing R] (x y : R) : (x + y)^3 = x^3 + 3*x^2*y + 3*x*y^2 + y^3 := by
   ring
 
 /-!
-  **Example** from the introduction of [Theorem Proving in Lean](https://lean-lang.org/theorem_proving_in_lean4/Introduction/#Intro)
+## And Commutative
 
-  Declares a theorem named `and_commutative` stating that for any propositions
-  `p` and `q`, `p ∧ q` implies `q ∧ p`. The `:=` begins the proof.
+This example is from the introduction of [Theorem Proving in
+Lean](https://lean-lang.org/theorem_proving_in_lean4/Introduction/#Intro)
+
+It declares a theorem named `and_commutative` stating that for any propositions
+`p` and `q`, `p ∧ q` implies `q ∧ p`. The `:=` begins the proof.
+
 -/
 theorem and_commutative (p q : Prop) : p ∧ q → q ∧ p :=
   -- Assumes the premise `p ∧ q` is true and binds its proof to the name `hpq`.
@@ -146,35 +162,34 @@ theorem and_commutative (p q : Prop) : p ∧ q → q ∧ p :=
 /-! ## Solving Word Problems -/
 
 /-!
-  **Problem 1**
+### Problem 1
 
-  The following is an example of how to solve a word problem.
+The following is an example of how to solve a word problem.
 
-  A plane flies 450km with the wind in 3 hours.
+A plane flies 450km with the wind in 3 hours.
 
-  Flying back against the wind, the plance takes 5 hours to make the trip.
+Flying back against the wind, the plance takes 5 hours to make the trip.
 
-  Prove that the plane's speed in still air is 120km/h.
+Prove that the plane's speed in still air is 120km/h.
 
-  We know that $d = v × t$.
+We know that $d = v × t$.
 
-  Let $x$ = plane's speed and $y$ = wind's speed.
+Let $x$ = plane's speed and $y$ = wind's speed.
 
-  For the first trip we have:  $h1: 450 = 3 × (x + y)$
+For the first trip we have:  $h1: 450 = 3 × (x + y)$
 
-  For the second trip we have: $h2: 450 = 5 × (x - y)$
+For the second trip we have: $h2: 450 = 5 × (x - y)$
 
-  Simplify to:
+Simplify to:
+- $h1: 150 = x + y$
+- $h2: 90 = x - y$
 
-  - $h1: 150 = x + y$
-  - $h2: 90 = x - y$
+What we want is to calculate the planes wind speed in still air ($x$).
 
-  What we want is to calculate the planes wind speed in still air ($x$).
+Answer:
+- $x = 120$
+- $y = 150-120 = 30$
 
-  Answer:
-
-  - $x = 120$
-  - $y = 150-120 = 30$
 -/
 example {x y : ℝ}
   (h1: x + y = 150)
@@ -196,18 +211,18 @@ example {x y : ℝ}
   exact ⟨h_x, h_y⟩
 
 /-!
-  **Problem 2**
+### Problem 2
 
-  A resistor has a resistance of 4 ohms
-  and a current of 3 Amps flows through it.
-  Prove the voltage across the resistor is 12 Volts.
+A resistor has a resistance of 4 ohms and a current of 3 Amps flows through it.
+Prove the voltage across the resistor is 12 Volts.
 
-  v = I × R
+$$v = I × R$$
 
-  where:
-  - v = Voltage (in Volts)
-  - I = Current (in Amps)
-  - R = Resistance (in Ohms)
+Where:
+- $v$ = Voltage (in Volts)
+- $I$ = Current (in Amps)
+- $R$ = Resistance (in Ohms)
+
 -/
 example {v I R : ℝ}
   (h1 : I = 3)
@@ -225,19 +240,20 @@ example {v I R : ℝ}
       norm_num
 
 /-!
-  **Problem 3**
+### Problem 3
 
-  Given the equations:
-  - $a - b = 4$
-  - $a × b = 1$
+Given:
+- $a - b = 4$
+- $a × b = 1$
 
-  Prove that $(a + b)^2 = 20$
+Prove that $(a + b)^2 = 20$
 
-  We know that $(a + b)^2 = (a - b)^2 + 4 × (a × b)$
+We know that $(a + b)^2 = (a - b)^2 + 4 × (a × b)$
 
-  From [The Mechanics of Proof
-  (MoP)](https://hrmacbeth.github.io/math2001/index.html), problem
-  [1.2.1](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id11)
+From [The Mechanics of Proof
+(MoP)](https://hrmacbeth.github.io/math2001/index.html), problem
+[1.2.1](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id11)
+
 -/
 example {a b : ℚ}
   (h1 : a - b = 4)
@@ -249,10 +265,11 @@ example {a b : ℚ}
     _ = 20 := by norm_num -- numerical calculation
 
 /-!
-  **Problem 4**
+### Problem 4
 
-  From [MoP](https://hrmacbeth.github.io/math2001/index.html), problem
-  [1.2.4](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id15)
+From [MoP](https://hrmacbeth.github.io/math2001/index.html), problem
+[1.2.4](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id15)
+
 -/
 example {a b c d e f : ℤ}
   (h1 : a * d = b * c)
@@ -264,12 +281,13 @@ example {a b c d e f : ℤ}
     _ = 0 := by ring
 
 /-!
-  **Problem 5**
+### Problem 5
 
-  A toy mouse changes speed from 2 m/s to 0 m/s in the span of 2 seconds.
-  It's mass is estimated to be around 0.1 kg.
-  The toy can only handle 2 Netwons of force (otherwise it breaks).
-  Prove that the force it experiences in stopping is below that limit.
+A toy mouse changes speed from 2 m/s to 0 m/s in the span of 2 seconds. It's
+mass is estimated to be around 0.1 kg. The toy can only handle 2 Netwons of
+force (otherwise it breaks). Prove that the force it experiences in stopping is
+below that limit.
+
 -/
 example {f v₀ v₁ m t a : ℝ}
   (h1 : v₀ = 2)
@@ -286,10 +304,11 @@ example {f v₀ v₁ m t a : ℝ}
     _ < 2 := by linarith -- as the calculation is deterministic
 
 /-!
-  **Problem 6**
+### Problem 6
 
-  From [MoP](https://hrmacbeth.github.io/math2001/index.html), problem
-  [1.4.1](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id33)
+From [MoP](https://hrmacbeth.github.io/math2001/index.html), problem
+[1.4.1](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id33)
+
 -/
 example {x y : ℤ}
   (hx : x + 3 ≤ 2)
@@ -305,10 +324,11 @@ example {x y : ℤ}
     -- y > 3 := by linarith
 
 /-!
-  **Problem 7**
+### Problem 7
 
-  From [MoP](https://hrmacbeth.github.io/math2001/index.html), problem
-  [1.4.2](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id35)
+From [MoP](https://hrmacbeth.github.io/math2001/index.html), problem
+[1.4.2](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id35)
+
 -/
 example {r s : ℚ} (h1 : s + 3 ≥ r) (h2 : s + r ≤ 3) : r ≤ 3 :=
   calc
@@ -317,10 +337,11 @@ example {r s : ℚ} (h1 : s + 3 ≥ r) (h2 : s + r ≤ 3) : r ≤ 3 :=
     _ = 3 := by linarith -- deterministic calculation
 
 /-!
-  **Problem 8.1**
+### Problem 8.1
 
-  From [MoP](https://hrmacbeth.github.io/math2001/index.html), problem
-  [1.4.3](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id37)
+From [MoP](https://hrmacbeth.github.io/math2001/index.html), problem
+[1.4.3](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id37)
+
 -/
 #count_heartbeats in
 example {x y : ℝ}
@@ -334,10 +355,11 @@ example {x y : ℝ}
     _ < 2 := by norm_num -- deterministic calculation
 
 /-!
-  **Problem 8.2**
+### Problem 8.2
 
-  From [MoP](https://hrmacbeth.github.io/math2001/index.html), problem
-  [1.4.3](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id37)
+From [MoP](https://hrmacbeth.github.io/math2001/index.html), problem
+[1.4.3](https://hrmacbeth.github.io/math2001/01_Proofs_by_Calculation.html#id37)
+
 -/
 #count_heartbeats in
 example {x y : ℝ}
@@ -348,11 +370,12 @@ example {x y : ℝ}
     x + y < 2 := by linarith
 
 /-!
-  **Floating Point Values**
+## Floating Point Values
 
-  Floating point values are not exact.
+Floating point values are not exact.
 
-  See https://0.30000000000000004.com/
+See <https://0.30000000000000004.com/>
+
 -/
 #eval (0.1 : Float) + (0.2 : Float) == (0.3 : Float)
 -- false
---
+++ Structure/Structure.lean
@@ -0,0 +1,243 @@
+import Mathlib.Algebra.Group.Defs
+import Mathlib.Analysis.Real.Sqrt
+import Mathlib.Data.Real.Basic
+import Mathlib.Order.Defs.PartialOrder
+import Mathlib.Tactic.Linarith
+import Mathlib.Tactic.NormNum
+import Mathlib.Tactic.Positivity
+import Mathlib.Tactic.Ring
+-- import Plausible
+
+namespace Structure
+
+/-!
+# Maths: Proofs with Structure
+
+Examples from [MoP](https://hrmacbeth.github.io/math2001/index.html), [Chapter
+2 Proofs with
+Structure](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html)
+
+## Plausible Tactic
+
+The [plausible tactic](https://api.cslib.io/docs/Plausible/Tactic.html) is a
+property testing tool (similar to QuickCheck). It works by randomly generating
+instances to search for counterexamples.
+
+Currently, the Plausible library in Lean 4 does not include a built-in random
+generator (SampleableExt) for the rational numbers (ℚ). Because it does not
+know how to generate random rationals, it fails with an error.
+
+To test hypotheses using plausible, you can temporarily change the types from ℚ
+to integers (ℤ), which it can generate:
+
+```lean4
+example {r s : ℤ}
+  (h1 : s + 3 ≥ r)
+  (h2 : s + r ≤ 3)
+  : r ≤ 3 := by plausible
+```
+
+-/
+-- example {r s : ℤ}
+--   (h1 : s + 3 ≥ r)
+--   (h2 : s + r ≤ 3)
+--   : r ≤ 3 := by plausible
+
+/-!
+## [2. 1 Intermediate Steps](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html#intermediate-steps)
+
+### [2.1.1](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html#example)
+
+Let $a$ and $b$ be real numbers and suppose that
+$a - 5b = 4$ and $b + 2 = 3$. Show that $a = 9$.
+
+-/
+example {a b : ℝ}
+  (h1 : a - 5 * b = 4)
+  (h2 : b + 2 = 3)
+  : a = 9 := by
+  have hb : b = 1 := by linarith [h2]
+  calc
+    a = a - 5 * b + 5 * b := by ring
+    _ = 4 + 5 := by rw [h1, hb]; simp
+    _ = 9 := by norm_num
+
+/-!
+### [2.1.1](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html#example)
+
+Simplified proof using
+[linarith](https://lean-lang.org/doc/reference/latest/The--grind--tactic/Linear-Arithmetic-Solver/).
+
+-/
+example {a b : ℝ}
+  (h1 : a - 5 * b = 4)
+  (h2 : b + 2 = 3)
+  : a = 9 := by linarith
+
+/-!
+### [2.1.3](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html#id3)
+
+Let $s$ and $r$ be rational numbers, and suppose that $s + 3 \ge r$ and $s + r
+\le 3$. Show that $r \le 3$.
+
+Introducing intermediate hypothesis using `have` avoids the [principle of
+logical explosion](https://en.wikipedia.org/wiki/Principle_of_explosion).
+
+-/
+example {r s : ℚ}
+  (h1 : s + 3 ≥ r)
+  (h2 : s + r ≤ 3)
+  : r ≤ 3 := by
+  have hr : r = (r + r) / 2 := by ring
+  have hs : r ≤ 3 - s := by linarith
+  calc
+    r = (r + r) / 2 := hr -- by hypothesis hr
+    _ ≤ ((s + 3) + (3 - s)) / 2 := by rel [h1, hs]
+    _ = 3 := by ring
+
+/-!
+## [2.2 Invoking Lemmas](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html#invoking-lemmas)
+
+### [2.2.1](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html#id9)
+
+Let $x$ be a rational number, and suppose that $3x = 2$. Show that $x ≠ 1$.
+
+Here we will use an existing lemma,
+[ne_of_lt](https://leanprover-community.github.io/mathlib4_docs/Mathlib/Order/Defs/PartialOrder.html#ne_of_lt)
+
+-/
+example {x : ℚ}
+  (hx : 3 * x = 2)
+  : x ≠ 1 := by
+  apply ne_of_lt  -- apply lemma convert ≠ to an inequality
+  calc
+    x = 3 * x / 3 := by ring -- introduce relationship so we can use hx
+    _ = 2 / 3 := by rw [hx] -- substitute hx
+    _ < 1 := by norm_num -- prove inequality using `norm_num`
+
+/-!
+### [2.2.2](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html#ne-of-gt)
+
+Let $y$ be a real number. Show that $y^2 + 1 ≠ 0$.
+
+It suffices to prove that $0 < y^2 + 1$, which is clear since squares are
+non-negative.
+
+Here we will use an existing lemma,
+[ne_of_gt](https://leanprover-community.github.io/mathlib4_docs/Mathlib/Order/Defs/PartialOrder.html#ne_of_gt)
+
+-/
+example {y : ℝ} : y ^ 2 + 1 ≠ 0 := by
+  apply ne_of_gt
+  have hy : y ^ 2 ≥ 0 := sq_nonneg y -- squares are non-negative
+  calc
+    y ^ 2 + 1 ≥ 0 + 1 := by rel [hy] -- substitute hy
+    _ > 0 := by norm_num -- prove inequality using `norm_num`
+
+/-!
+Can also be proven with the
+[positivity](https://leanprover-community.github.io/mathlib4_docs/Mathlib/Tactic/Positivity/Core.html#positivity)
+tactic.
+
+-/
+example {y : ℝ} : y ^ 2 + 1 ≠ 0 := by
+  positivity
+
+/-!
+Checking $√2 ≤ 2 by applying
+[sqrt_le_iff](https://leanprover-community.github.io/mathlib4_docs/Mathlib/Analysis/SpecialFunctions/Sqrt.html#Real.sqrt_le_iff)
+lemma.
+
+This applies the theorem to the `norm_num` tactic.
+
+-/
+example : Real.sqrt 2 ≤ 2 := by
+  norm_num[Real.sqrt_le_iff]
+
+/-!
+We can also `apply` the lemma using `mpr`, where:
+
+Real.sqrt_le_iff.mpr : (0 ≤ y ∧ x ≤ y ^ 2) → Real.sqrt x ≤ y
+
+`mpr`: is a projection (lemma) that stands for "modus ponens reverse"
+
+-/
+example : Real.sqrt 2 ≤ 2 := by
+  apply Real.sqrt_le_iff.mpr  -- apply lemma using mpr
+  norm_num -- prove inequality using `norm_num`
+
+/-!
+### [2.1.2](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html#id2)
+Let $m$ and $n$ be integers, and suppose that $m + 3 ≤ 2n -1$ and $n ≤ 5$.
+Show that $m ≤ 6$.
+-/
+example {m n : ℤ} (h1 : m + 3 ≤ 2 * n - 1) (h2 : n ≤ 5) : m ≤ 6 := by
+  have h3 : m ≤ 2 * n - 4 := by linarith [h1]
+  calc
+    m ≤ 2 * n - 4 := h3 -- use h3
+    _ ≤ 2 * 5 - 4 := by rel [h2] -- substitute h2
+    _ = 6 := by norm_num -- prove inequality using `norm_num`
+
+/-!
+Solution **2.1.2** given in book.
+-/
+example {m n : ℤ} (h1 : m + 3 ≤ 2 * n - 1) (h2 : n ≤ 5) : m ≤ 6 := by
+  have h3 :=
+  calc
+    m + 3 ≤ 2 * n - 1 := by rel [h1]
+    _ ≤ 2 * 5 - 1 := by rel [h2]
+    _ = 9 := by norm_num
+  linarith [h3]
+
+/-!
+### [2.1.3](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html#id3)
+
+Let $r$ and $s$ be rational numbers, and suppose that $s + 3 \ge r$ and $s + r \le 3$. Show that $r \le 3$.
+
+Here we introduce the following intermediate hypotheses:
+
+$$h3: r ≤ 3 - s$$
+
+-/
+example {r s : ℚ} (h1 : s + 3 ≥ r) (h2 : s + r ≤ 3) : r ≤ 3 := by
+  have
+    h3 : r ≤ 3 - s := by linarith [h2] -- substitute h2
+  calc
+    r = (r + r) / 2 := by ring
+    _ <= (3 + s + 3 - s) / 2 := by linarith [h2, h3]
+    _ = 3 := by ring
+
+/-!
+### [2.1.4](https://hrmacbeth.github.io/math2001/02_Proofs_with_Structure.html#id4)
+
+Let $t$ be a real number, and suppose that $t^2 = 3t$ and $t ≥ 1$. Show that in fact $t ≥ 2$.
+-/
+example {t : ℝ} (h1 : t ^ 2 = 3 * t) (h2 : t ≥ 1) : t ≥ 2 := by
+  have h3 : t * t = t * 3 := by
+    calc
+      t * t = t ^ 2 := by ring
+      _ = 3 * t := h1
+      _ = t * 3 := by ring
+  have ht : t ≠ 0 := by linarith [h2]
+  have h4 : t = 3 := mul_left_cancel₀ ht h3
+  linarith [h4]
+
+/-!
+Simplified solution to **2.1.4**.
+-/
+example {t : ℝ} (h1 : t ^ 2 = 3 * t) (h2 : t ≥ 1) : t ≥ 2 := by
+  nlinarith
+
+/-!
+Alternative that shows exact value of $t : t = 3$.
+-/
+example {t : ℝ} (h1 : t ^ 2 = 3 * t) (h2 : t ≥ 1) : t = 3 := by
+  have h3 : t * t = t * 3 := by
+    calc
+      t * t = t ^ 2 := by ring
+      _ = 3 * t := h1
+      _ = t * 3 := by ring
+  have ht : t ≠ 0 := by linarith [h2]
+  exact mul_left_cancel₀ ht h3
+
+end Structure
